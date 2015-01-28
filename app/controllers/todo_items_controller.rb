70e080b592bd8e4f9f4e0fe324adf7ac16237b80
thedesignable <himanshu-patel@live.com>
2015-01-29 02:35:41 +0530

    Ability to mark as completed done

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,5 +1,6 @@
 class TodoItemsController < ApplicationController
 	before_action :set_todo_list
+	before_action :set_todo_item, except: (:create)
 
 	def create 
 		@todo_item = @todo_list.todo_items.create(todo_item_params)
@@ -7,7 +8,6 @@ class TodoItemsController < ApplicationController
 	end	
 
 	def destroy
-		@todo_item = @todo_list.todo_items.find(params[:id])
 		if @todo_item.destroy
 			flash[:success] = "Deletion Successful"
 		else
@@ -15,6 +15,13 @@ class TodoItemsController < ApplicationController
 		end
 		redirect_to @todo_list
 	end		
+
+	def complete
+		@todo_item.update_attribute(:completed_at, Time.now)
+		redirect_to @todo_list, notice: "yay! you completed it
+		"
+	end	
+
 
 	private
 
@@ -25,4 +32,8 @@ class TodoItemsController < ApplicationController
 	def todo_item_params
 		params[:todo_item].permit(:content)
 	end	
+
+	def set_todo_item
+		@todo_item = @todo_list.todo_items.find(params[:id])
+	end
 end
